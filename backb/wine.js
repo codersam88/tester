bdd6ab06d2c0cbc4adc810a29dadbc1a84153dc3
sampath surineni <sampath.skt@gmail.com>
2016-01-21 15:53:40 +0530

    Done basics

--- backb/wine.js
+++ backb/wine.js
@@ -3,3 +3,27 @@ Wine = Backbone.Model.extend();
 firstWine = new Wine ({
     name: "Clos Pegase"
 });
+
+Wines = Backbone.Collection.extend ({
+    Model: Wine,
+    url: '#'
+}); 
+
+Wines = new Wines ([
+    {name: 'Robert Mondovi'},
+    {name: 'CakeBread'}
+]);
+
+HomeView = Backbone.View.extend ({
+    el:'body',
+    
+    initialize: function() {
+	this.render();
+    },
+    
+    render: function() {
+	this.$el.empty();
+	this.$el.append('<h1>My App</h1>');
+	return this;
+    }
+});
